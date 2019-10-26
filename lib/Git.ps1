28352d9e548ab34cce1736971baa3fb35d6dfb79
Chloé <rightfold+ck@gmail.com>
2019-10-26 12:14:02 +0200

    Properly deal with Git errors

--- lib/Git.ps1
+++ lib/Git.ps1
@@ -4,20 +4,25 @@
 # functions assume a remote called origin, and will keep this remote up-to-date
 # with the local changes they make, and possibly vice versa.
 
-# TODO: Gracefully handle Git commands failing. Do not leave the repository in
-# a dirty state.
-
-# TODO: If Git fails, report a PowerShell error.
-
 function Git-Fetch {
     git fetch
+    if (!$?) {
+        Throw "git fetch"
+    }
 }
 
 function Git-Reset {
     param ([Parameter(Mandatory)] [string] $Branch)
 
     git checkout $Branch
+    if (!$?) {
+        Throw "git checkout"
+    }
+
     git reset --hard "origin/$Branch"
+    if (!$?) {
+        Throw "git reset"
+    }
 }
 
 function Git-Rebase {
@@ -27,8 +32,20 @@ function Git-Rebase {
     )
 
     git checkout $Head
+    if (!$?) {
+        Throw "git checkout"
+    }
+
     git rebase $Base
+    if (!$?) {
+        git rebase --abort
+        Throw "git rebase"
+    }
+
     git push --force-with-lease origin $Head
+    if (!$?) {
+        Throw "git push"
+    }
 }
 
 function Git-Merge {
@@ -38,6 +55,18 @@ function Git-Merge {
     )
 
     git checkout $Base
+    if (!$?) {
+        Throw "git checkout"
+    }
+
     git merge --no-edit --no-ff $Head
+    if (!$?) {
+        git merge --abort
+        Throw "git merge"
+    }
+
     git push origin $Base
+    if (!$?) {
+        Throw "git push"
+    }
 }
